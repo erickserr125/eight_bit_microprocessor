cfd9125ec9bc6d51ce94b46a27c14084f1aae966
erickserr125 <48919139+erickserr125@users.noreply.github.com>
2022-05-05 15:17:58 -0700

    Delete imem.v

--- imem.v
+++
@@ -1,8 +0,0 @@
-module imem (input [7:0] a, output [2:0] rd);
-reg [2:0] RAM[63:0]; // limited memory
-initial
-begin
-$readmemh ("memfile.dat",RAM);//FINISHED
-end
-assign rd = RAM[a]; // word aligned
-endmodule
